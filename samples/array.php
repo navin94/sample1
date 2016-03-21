625ce2e3473d4af022898b2b748a400c5c8ab77a
nkumar <nkumar@mountbees.com>
2016-03-21 10:57:31 +0530

    first change

--- samples/array.php
+++ samples/array.php
@@ -1,6 +1,6 @@
 <?php
 /* First method to create array. */
-$numbers = array( 1, 2, 3, 4, 5);
+$numbers = array( 1, 2, 3, 4, 5, 6);
 foreach( $numbers as $value )
 {
 	echo "Value is $value <br />";
@@ -11,6 +11,7 @@ $numbers[1] = "two";
 $numbers[2] = "three";
 $numbers[3] = "four";
 $numbers[4] = "five";
+$numbers[5] = "six";
 foreach( $numbers as $value )
 {
 	echo "Value is $value <br />";
